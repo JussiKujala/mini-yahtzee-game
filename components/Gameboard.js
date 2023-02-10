fe80dcab959a2e82c9c530f43a23605b1c72e530
Jussi Kujala <jussijustus@gmail.com>
2023-02-10 09:47:44 +0200

    modified:   App.js 	new file:   components/Footer.js 	new file:   components/Gameboard.js 	new file:   components/Header.js 	new file:   components/Home.js 	new file:   components/Scoreboard.js 	new file:   constants/Game.js 	modified:   package-lock.json 	modified:   package.json 	new file:   style/style.js

---
+++ components/Gameboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from '../style/style';
+
+export default Gameboard = () => {
+  return (
+    <View>
+      <Text>
+        Gameboard will be here..
+      </Text>
+    </View>
+  )
+}
---
+++ components/Home.js
@@ -0,0 +1,41 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { Text, View, Pressable, Keyboard, TextInput } from 'react-native';
+import styles from '../style/style';
+
+export default Home = () => {
+
+    const [playerName, setPlayerName] = useState('');
+    const [hasPlayerName, setHasPlayerName] = useState(false)
+
+    const handlePlayerName = (value) => {
+        if (value.trim().length > 0){
+            setHasPlayerName(true);
+            Keyboard.dismiss();
+        }
+    }
+
+  return (
+    <View>
+        <Text>Home is here</Text>
+        { !hasPlayerName 
+        ?
+        <>
+        <Text>For Scoreboard enter your name</Text>
+        <TextInput onChangeText = {setPlayerName} autoFocus={true}></TextInput>
+        <Pressable onPress={() => handlePlayerName(playerName)}>
+            <Text>OK</Text>
+        </Pressable>
+        </>
+        :
+        <>
+        <Text>Rules of the game here..</Text>
+        <Text>Good luck, {playerName}</Text>
+        <Pressable>
+            <Text>PLAY</Text>
+        </Pressable>
+        </>
+        }
+    </View>
+  )
+}
