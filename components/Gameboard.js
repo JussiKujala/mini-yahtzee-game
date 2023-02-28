33981849ce72e3adbbe6bbe6e126c08362f32cdf
Jussi Kujala <jussijustus@gmail.com>
2023-02-28 14:13:08 +0200

    modified:   components/Gameboard.js

--- components/Gameboard.js
+++ components/Gameboard.js
@@ -17,13 +17,13 @@ export default Gameboard = ({ route }) => {
   //this array has the information whether dice is selected or not
   const [selectedDices, setSelectedDices] =
     useState(new Array(NBR_OF_DICES).fill(false));
-//this arrray has the information wheter the spot count has been selected or not
+  //this arrray has the information wheter the spot count has been selected or not
   const [selectedDicePoints, setSelectedDicePoints] =
     useState(new Array(MAX_SPOT).fill(false));
-//this array has total points for different spot counts
+  //this array has total points for different spot counts
   const [diceSpots, setDiceSpots] = useState(new Array(NBR_OF_DICES).fill(0));
   const [dicePointsTotal, setDicePointsTotal] = useState(new Array(MAX_SPOT).fill(0));
-  
+
 
   const row = [];
   for (let i = 0; i < NBR_OF_DICES; i++) {
@@ -42,29 +42,32 @@ export default Gameboard = ({ route }) => {
   }
 
   const pointsRow = [];
-  
-for (let spot = 0; spot < MAX_SPOT; spot++){
-  pointsRow.push(
-    <Col key={"points" + spot}>
-      <Text key={"points" + spot} style={styles.points}>0</Text>
-    </Col>
-  )
-}
-  
 
-const ButtonsRow = [];
-for(let diceButton = 0; diceButton < MAX_SPOT; diceButton++ ){
-  ButtonsRow.push(
-  <Col key={"buttonsRow" + diceButton}>
-    <Pressable key={"buttonsRow" + diceButton}>
-    <MaterialCommunityIcons name={"numeric-" + (diceButton + 1) + "-circle"}
-    key={"buttonsRow" + diceButton} size={40} color={"steelblue"}
-    >
-    </MaterialCommunityIcons>
-    </Pressable>
-  </Col>
-  )
-}
+  for (let spot = 0; spot < MAX_SPOT; spot++) {
+    pointsRow.push(
+      <Col key={"points" + spot}>
+        <Text key={"points" + spot} style={styles.points}>{getSpotTotal(spot)}</Text>
+      </Col>
+    )
+  }
+
+
+  const ButtonsRow = [];
+  for (let diceButton = 0; diceButton < MAX_SPOT; diceButton++) {
+    ButtonsRow.push(
+      <Col key={"buttonsRow" + diceButton}>
+        <Pressable
+          onPress={() => selectDicePoints(diceButton)}
+          key={"buttonsRow" + diceButton}>
+          <MaterialCommunityIcons name={"numeric-" + (diceButton + 1) + "-circle"}
+            key={"buttonsRow" + diceButton}
+            size={40}
+            color={getDicePointsColor(diceButton)}
+          ></MaterialCommunityIcons>
+        </Pressable>
+      </Col>
+    )
+  }
 
   useEffect(() => {
     if (playerName === '' && route.params?.player) {
@@ -72,12 +75,16 @@ for(let diceButton = 0; diceButton < MAX_SPOT; diceButton++ ){
     }
   }, []);
 
-  function getDiceColor(i){
-    if (board.every((val, i, arr) => val === arr[0])){
-      return "orange";
+  function getDiceColor(i) {
+    return selectedDices[i] ? "black" : "steelblue"
+  }
+
+  function getDicePointsColor(i) {
+    if (selectedDicePoints[i]) {
+      return "black";
     }
-    else{
-      return selectedDices[i] ? "black" : "steelblue"
+    else {
+      return "steelblue";
     }
   }
 
@@ -87,27 +94,53 @@ for(let diceButton = 0; diceButton < MAX_SPOT; diceButton++ ){
     setSelectedDices(dices);
   }
 
+function getSpotTotal(i){
+  return dicePointsTotal[i];
+}
+
+  function selectDicePoints(i) {
+    let selected = [...selectedDices];
+    let selectedPoints = [...selectedDicePoints];
+    let points = [...dicePointsTotal];
+    if (!selectedPoints[i]) {
+      selectedPoints[i] = true;
+      let nbrOfDices = diceSpots.reduce((total, x) => (x === (i + 1) ? total + 1 : total), 0);
+      points[i] = nbrOfDices * (i + 1);
+      setDicePointsTotal(points);
+    }
+    selected.fill(false);
+    setSelectedDices(selected);
+    setSelectedDicePoints(selectedPoints);
+    setNbrOfThrowsleft(NBR_OF_THROWS);
+    return points[i];
+  }
+
   const throwDices = () => {
+    let spots = [...diceSpots];
     for (let i = 0; i < NBR_OF_DICES; i++) {
-      if(!selectedDices[i]){
-      let randomNumber = Math.floor(Math.random() * 6 + 1);
-      board[i] = 'dice-' + randomNumber;
+      if (!selectedDices[i]) {
+        let randomNumber = Math.floor(Math.random() * 6 + 1);
+        board[i] = 'dice-' + randomNumber;
+        spots[i] = randomNumber;
       }
     }
-    setNbrOfThrowsleft(nbrOfThrowsleft-1);
+    setNbrOfThrowsleft(nbrOfThrowsleft - 1);
+    setDiceSpots(spots);
+    setStatus('Select and throw dices again');
   }
 
   return (
     <View style={styles.gameboard}>
       <View style={styles.flex}>{row}</View>
       <Text style={styles.gameinfo}>Throws left: {nbrOfThrowsleft}</Text>
-        <Text style={styles.gameinfo}>{status}</Text>
-        <Pressable style={styles.button} onPress={() => throwDices()}>
-          <Text style={styles.buttonText}>Throw dices</Text>
-        </Pressable>
-        <View style={styles.dicepoints}><Grid>{pointsRow}</Grid></View>
-        <View style={styles.dicepoints}><Grid>{ButtonsRow}</Grid></View>
+      <Text style={styles.gameinfo}>{status}</Text>
+      <Pressable style={styles.button} onPress={() => throwDices()}>
+        <Text style={styles.buttonText}>Throw dices</Text>
+      </Pressable>
+      <View style={styles.dicepoints}><Grid>{pointsRow}</Grid></View>
+      <View style={styles.dicepoints}><Grid>{ButtonsRow}</Grid></View>
       <Text>Player: {playerName}</Text>
     </View>
   )
 }
+
